c4a84ea943c4202c514340d0f59fd614058091bf
Pulkit Sinha <itsapinhulk@outlook.com>
2026-01-18 22:50:07 -0500

    Add build time

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
 	plugins: [viteSingleFile()],
+	define: {
+		__BUILD_TIME__: JSON.stringify(Date.now()),
+	}
 })
